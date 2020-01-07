77a46a6695c1dabbc91a477611909d5dc372f749
dxnter <contact@danfoster.io>
2020-01-07 15:58:48 -0600

    Added error handling functions for invalid users and period paramaters

--- utils/util.js
+++ utils/util.js
@@ -1,13 +1,23 @@
-module.exports = class Util {
+const { PERIOD_PARAMS } = require('../constants');
+
+class Util {
   static delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
 
-  static extension(reaction, attachment) {
-    const imageLink = attachment.split('.');
-    const typeOfImage = imageLink[imageLink.length - 1];
-    const image = /(jpg|jpeg|png|gif)/gi.test(typeOfImage);
-    if (!image) return '';
-    return attachment;
+  static userNotSet(message, args) {
+    return {
+      author: 'Error',
+      description: `Last.FM username not set, enter \`,lf set <username>\` or enter a username after \`${args[0]}\``
+    };
   }
-};
+
+  static invalidPeriod(period) {
+    return {
+      author: 'Error',
+      description: `Invalid period: **${period}**\nPeriods:  \`week\`, \`month\`, \`90\`, \`180\`, \`year\`, \`all\` (Default: all)`
+    };
+  }
+}
+
+export { Util };
